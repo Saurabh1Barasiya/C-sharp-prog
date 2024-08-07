a132bdbc4d651bc451321bcce871a3e0c4dcb6d3
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-07 11:52:00 +0530

    First hello world program.

---
+++ Part-1/PrintHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 👀 print the hello world program.
+            // Console is present inside System namespace.
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
