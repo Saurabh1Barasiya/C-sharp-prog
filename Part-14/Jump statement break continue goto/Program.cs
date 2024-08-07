21c17ae91ae772091fd5ed792012b31066290297
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-07 17:53:12 +0530

    jump statement break,continue,goto.

---
+++ Part-14/Jump statement break continue goto/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jump_statement_break_continue_goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("break");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("continue");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i <= 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+
+            // goto.
+            Console.WriteLine("go to...");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    goto jump;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("one...");
+            Console.WriteLine("two...");
+            Console.WriteLine("three...");
+
+            // its a label for goto. statement.
+            jump:
+                Console.WriteLine("done");
+
+
+            Console.ReadKey();
+        }
+    }
+}
