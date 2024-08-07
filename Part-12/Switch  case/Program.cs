fb65bf0589cae5101ecd7ace24c7129c1c1144e6
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-07 17:01:00 +0530

    Switch case .

---
+++ Part-12/Switch  case/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch__case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the operator");
+
+            string op = Console.ReadLine();
+
+            switch (op){
+                case "+":
+                        Console.WriteLine("add");
+                        break;
+                case "-":
+                        Console.WriteLine("subtract");
+                    break;
+                case "*":
+                        Console.WriteLine("multiply");
+                        break;
+                case "/":
+                        Console.WriteLine("divide");
+                        break;
+                default:
+                       Console.WriteLine("Invalid Operator...");
+                        break ;
+            }
+            Console.ReadLine();
+        }
+    }
+}
