15aa02e32239730982361e5160e069dca4224b93
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-07 16:13:15 +0530

    if - else

---
+++ Part-10/IfBlock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example 1");
+            if (5>1)
+            {
+                Console.WriteLine("Inside if block");
+            }
+            else
+            {
+                Console.WriteLine("Else block");
+            }
+
+
+
+            Console.WriteLine("Example 2");
+            if (false)
+            {
+                Console.WriteLine("Inside if block");
+            }
+            else
+            {
+                Console.WriteLine("Else block");
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
