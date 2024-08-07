da5dc7d72d7bbd924926c1a61f0c3f248acfb7ec
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-07 12:12:32 +0530

    basic program that take name and age and print.

---
+++ Part-3/takingValueAndPrint/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takingValueAndPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+
+            Console.ReadLine();
+        }
+    }
+}
