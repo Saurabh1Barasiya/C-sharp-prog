5ffb7cf7cf0c5797f85f89d86973da543b4a61ba
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-08 17:31:37 +0530

    Constructor theory.

---
+++ Part-21/Constructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // In C#, a constructor is a special method in a class that is called when an instance of the class is created.
+            // Constructors are used to initialize the object's state by setting initial values for its fields or performing any setup required.
+
+            // Key Features of Constructors.
+
+            // A constructor must have the same name as the class in which it is defined.
+            // Constructors do not have a return type, not even void.
+            // Constructors are called automatically when an object of the class is instantiated.
+            // You can define multiple constructors in a class with different parameters (constructor overloading).
+            // If no constructor is defined, the compiler provides a default constructor that initializes fields to their default values.
+
+
+
+            // Constructors initialize objects and set up initial values.
+            // They can be default, parameterized, static, or copy constructors.
+            // Constructor overloading allows multiple constructors with different parameters.
+            // Constructor chaining allows constructors to call each other using this.
+
+
+        }
+    }
+}
