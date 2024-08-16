2208bacfecced84980888b3e2c2a64546757480d
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-16 16:54:03 +0530

    About Types of Inheritance.

---
+++ Part-32/Types of inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Single Inheritance
+            // Multilevel Inheritance
+            // Hierarchical Inheritance
+            // Multiple Inheritance (Not Directly Supported in C#).
+            // Hybrid Inheritance (Not Directly Supported in C#)
+
+
+            // Single Inheritance: A class inherits from one base class.
+            // Multilevel Inheritance: A class inherits from a class that is already a derived class.
+                    // A class can inherit from more than one base class. This type of inheritance is not directly supported in C#.
+
+            // Hierarchical Inheritance: Multiple classes inherit from the same base class.
+                    //A combination of two or more types of inheritance.For example, a combination of hierarchical and multiple inheritance.
+
+        }
+    }
+}
