d8ec46af9c256d8b346d33f3c3d3c64fa91a6881
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-16 19:54:36 +0530

    base keyword constructor inheritance.

---
+++ Part-36/Constructor Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Inheritance
+{
+    public class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("I am Parent class Constructor...");
+        }
+    }
+
+    public class Child:Parent {
+
+        // base() ye likhne ki need nahi h, but
+        // agar parent me parameter wala constructor h to fir apko base() likhna padega...
+
+        public Child() : base()
+        { 
+            Console.WriteLine("I am Child class Constructor...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child obj = new Child();
+            Console.ReadLine();
+        }
+    }
+}
