23596fb0fe6a57a31effffdac22aa64183587fe4
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-16 20:46:19 +0530

    basic example of constructor inheritance.

---
+++ Part-38/Constructor Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Inheritance
+{
+    public class Human
+    {
+        public Human(string firstname, string lastname,string address, int age) {
+            Console.WriteLine($"firstname {firstname}");
+            Console.WriteLine($"lastname {lastname}");
+            Console.WriteLine($"lastname {address}");
+            Console.WriteLine($"age {age}");
+        } 
+    }
+
+    public class Teacher : Human
+    {
+        public Teacher(string firstname,string lastname,int age,string address,string teachertype,double salary) : base(firstname,lastname,address,age)
+        {
+            Console.WriteLine($"Teacher type {teachertype}");
+            Console.WriteLine($"Teacher salary {salary}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Saroj","nagwanshi",45,"Chhindwara","Primary",56000);
+            Console.ReadLine();
+        }
+    }
+}
