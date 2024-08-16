a7dcd9e67b9c70c729da68381d300143e28bf459
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-16 20:02:23 +0530

    base Contructor Inheritance.

---
+++ Part-37/Constructor Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Inheritance
+{
+    public class Parent
+    {
+        public Parent(string message)
+        {
+            Console.WriteLine($"I am Parent constructor...  {message}");
+        }
+    }
+
+    public class child : Parent {
+        public child() : base("ok") 
+        {
+            Console.WriteLine("I am child constructor...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            child obj = new child();
+            Console.ReadLine();
+        }
+    }
+}
