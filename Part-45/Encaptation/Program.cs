730a5432baec051fe181dee17a007e88fa2fe633
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-17 19:33:14 +0530

    Encaptation theory.

---
+++ Part-45/Encaptation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encaptation
+{
+
+    // Encapsulation is one of the fundamental principles of object-oriented programming(OOP).
+    // In C#, encapsulation is the concept of bundling data (fields) and methods (functions) that operate on the data into a single unit, typically a class.
+    // It also involves restricting direct access to some of the object's components, which is achieved by using access modifiers.
+    // This way, you can control how the data in your objects is accessed and modified.
+
+
+
+    // Encapsulation also involves restricting access to certain components of an object to ensure that the internal implementation
+    // details are hidden from the outside world, which is known as data hiding.
+
+
+    // in Encapsulation we can group data members and function to a single entity. like claass.
+    // class is mean to archive Encapsulation.
+
+    // Key Concepts of Encapsulation:
+    // Data Hiding:
+            // Encapsulation allows you to hide the internal state of an object and only expose the necessary parts to the outside world.
+            // This is achieved using access modifiers(like private, protected, internal, public).
+
+            // For example, by marking a field as private, it cannot be accessed directly from outside the class.
+            // Instead, you provide public methods(getters and setters) to access or modify the private data.
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
