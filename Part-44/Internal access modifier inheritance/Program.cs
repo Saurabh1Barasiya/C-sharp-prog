dc8b2521700786aeeaba01ec8c4e64778d8b2773
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-17 10:20:09 +0530

    Internal access modifier.

---
+++ Part-44/Internal access modifier inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal_access_modifier_inheritance
+{
+    public class Example
+    {
+        internal string name = "saurabh";
+
+        internal void showData()
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+
+    public class Test : Example { }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            Console.WriteLine(obj.name);
+
+            obj.showData();
+
+            Console.ReadLine();
+        }
+    }
+}
