3e177e937234581e63bc8a9031c3a3ca3a6c9371
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-19 15:19:32 +0530

    more static properties.

---
+++ Part-52/More on static properties/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_on_static_properties
+{
+    // can we get acces instance variable in static properties.
+
+    public class Example
+    {
+        public string name;
+        public static string _name
+        {
+            set {
+
+                // aesa nahi kar sakte h kyoki name intance variable h.
+                // _name 1  static property hai.
+                name = value;
+            }
+            get {
+                return name;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
