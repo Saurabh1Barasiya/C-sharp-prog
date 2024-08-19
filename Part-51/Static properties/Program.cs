81eea150e4af3c8707b77b8a483dd4a9e8f1ee14
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-19 15:07:39 +0530

    static properties .

---
+++ Part-51/Static properties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Static_properties
+{
+    public class Example
+    {
+        public static int count { get; private set; } = 0;
+        public Example()
+        {
+            count++;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // we cannot set value because set accessor is private.
+            // Example.count = 10;
+
+            Console.WriteLine($"first value when we want to access {Example.count}");
+
+            Example obj1 = new Example();
+            Example obj2 = new Example();
+            Example obj3 = new Example();
+            Example obj4 = new Example();
+
+
+            Console.WriteLine($"Final value of static variable : {Example.count}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
