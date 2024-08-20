ea2cdeaa3fb09b7e893d55a97b110a5c2e2d718d
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-20 19:05:12 +0530

    overloding program

---
+++ Part-56/Inheritance overloding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_overloding
+{
+    // Overloading by Order of Parameters.
+    // parameter are same but arragement was diffrent.
+
+    public class Example
+    {
+        public void show(int number,string message)
+        {
+            Console.WriteLine($"Number {number} and message {message} ");
+        } 
+        
+        public void show(string message, int number)
+        {
+            Console.WriteLine($"message {message} and Number {number} ");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            example.show(10, "I am first ");
+            example.show(11, "I am secound");
+            Console.ReadLine();
+        }
+    }
+}
