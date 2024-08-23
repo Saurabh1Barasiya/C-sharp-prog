df7e3f9c75b7f4133b2492ae54da9811a25ea265
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-23 17:50:17 +0530

    more on sealed class

--- Part-65/more on sealed class/Program.cs
+++ Part-65/more on sealed class/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace more_on_sealed_class
 {
 
+
+    // In typical object-oriented design, if you want to allow some inheritance but prevent further inheritance at a certain level,
+    // sealing a "middle" class (a class between the base and most derived classes) can make sense.
+
     public class BaseClass
     {
         public virtual void DisplayMessage()
@@ -29,6 +33,9 @@ namespace more_on_sealed_class
 
     }
 
+
+    // BaseClass: This is open for inheritance, allowing derived classes to extend its functionality.
+    // MiddleClass: This class is sealed, meaning no further inheritance is possible beyond this point.It provides a final, specific implementation that you don't want to be overridden or extended.
 
 
     internal class Program
