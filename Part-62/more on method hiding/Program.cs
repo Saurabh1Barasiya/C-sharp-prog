e77c0ac46dac83a3dbedda27a450af924f75c627
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-23 12:24:25 +0530

    method hiding.

---
+++ Part-62/more on method hiding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more_on_method_hiding
+{
+    public class Employee
+    {
+        public string Firstname;
+        public string Lastname;
+
+        public void ShowFullName()
+        {
+            Console.WriteLine($"First name {Firstname} and lastname {Lastname}");
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public new void ShowFullName()
+        {
+            Console.WriteLine($"First name {Firstname} and lastname {Lastname} PTE");
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public new void ShowFullName()
+        {
+            Console.WriteLine($"First name {Firstname} and lastname {Lastname} FTE");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployee parttimeemployee = new PartTimeEmployee();
+            parttimeemployee.Firstname = "Jhon";
+            parttimeemployee.Lastname = "Doe";
+            parttimeemployee.ShowFullName();
+            Console.ReadLine();
+        }
+    }
+}
