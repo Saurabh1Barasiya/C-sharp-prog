6759694c967569ce36f5e1e563e902dfd2b2b83b
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-29 20:43:09 +0530

    Multicast delegate.

---
+++ Part-68/Delegate basic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_basic
+{
+
+    // Example of multicast delegate...
+
+    public delegate void Show();
+
+    internal class Program
+    {
+        public static void Display()
+        {
+            Console.WriteLine("I am Display method...");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("I am Print method...");
+        }
+
+        public static void Dikhao()
+        {
+            Console.WriteLine("I am Dikhao method...");
+        }
+
+        static void Main(string[] args)
+        {
+            Show obj = Display;
+            obj += Print;
+            obj += Dikhao;
+            obj.Invoke();
+            Console.ReadLine();
+        }
+    }
+}
