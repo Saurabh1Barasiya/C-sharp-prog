c23a9669ef0f3312122ecd073a1f2c2cc7362aaa
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-29 21:04:41 +0530

    delegate -=

---
+++ Part-69/Delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+
+    public delegate void Show();
+
+    internal class Program
+    {
+
+        public static void Display()
+        {
+            Console.WriteLine("I am Display method...");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("I am Print method...");
+        }
+
+        public static void Dikhao()
+        {
+            Console.WriteLine("I am Dikhao method...");
+        }
+
+        static void Main(string[] args)
+        {
+            Show obj = Display;
+            obj += Print;
+
+            // To isme Dikhao wale nahi chagea 
+            // Display and print hi chalega.
+            obj -= Dikhao;
+            obj();
+
+            Console.ReadLine();
+        }
+    }
+}
