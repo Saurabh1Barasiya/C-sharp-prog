1830b5ffeb62ee3ff962045311cf3e7eb66636cd
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-10 08:58:41 +0530

    more on partial class.

---
+++ Part-71/more on partial class/Manager1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more_on_partial_class
+{
+    public partial class Manager : Employee
+    {
+        public string Department {  get; set; }
+
+        public override void Showdata()
+        {
+            Console.WriteLine($"Employee: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Age: {Department}");
+            Console.WriteLine($"Age: {Bonus}");
+        }
+    }
+}
---
+++ Part-71/more on partial class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more_on_partial_class
+{
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Showdata()
+        {
+            Console.WriteLine($"Employee: {Name}, Age: {Age}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.Name = "Alem";
+            manager.Age = 50;
+            manager.Department = "IT";
+            manager.Bonus = 50000.00;
+            manager.Showdata();
+
+            Console.ReadLine();
+
+        }
+    }
+}
