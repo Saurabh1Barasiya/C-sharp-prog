dc36b40a20837f73a298aa8156e76cf10c790052
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-10 23:37:05 +0530

    more on genric class.

---
+++ Part-83/More on Genric/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_on_Genric
+{
+    public class Pair<Tkey, Tvalue>
+    {
+        private Tkey Key;
+        private Tvalue Value;
+
+        public Pair(Tkey key, Tvalue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public Tkey Getkey()
+        {
+            return Key;
+        }
+
+        public Tvalue Getvalue() {
+            return  Value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, string> obj = new Pair<int, string>(1, "one");
+            Console.WriteLine($"Key is {obj.Getkey()}");
+            Console.WriteLine($"Value is {obj.Getvalue()}");
+
+            Console.ReadLine();
+        }
+    }
+}
