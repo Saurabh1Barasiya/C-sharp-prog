afd6bea7d4268a031ab038b59d9fd15f10ce1b3e
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-11 12:28:02 +0530

    Queue

---
+++ Part-87/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        // A collection that follows the FIFO(First In First Out) principle.
+        // You enqueue items at the end and dequeue from the front.
+
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue("two");
+            q.Enqueue(3.0);
+            q.Enqueue(55556.21453);
+            q.Enqueue('5');
+
+
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Dequeue());
+            Console.ReadLine();
+        }
+    }
+}
