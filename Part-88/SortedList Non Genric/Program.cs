269fb128aecc42df7ed732f71cd42f7aa7362269
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-11 12:37:02 +0530

    Sorted list.

---
+++ Part-88/SortedList Non Genric/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList_Non_Genric
+{
+    internal class Program
+    {
+        // A collection of key-value pairs that are sorted by the key.
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+            sl.Add(1, "one");
+            sl.Add(2, "two");
+            sl.Add(3, "three");
+
+
+            Console.WriteLine(sl[1]);
+            Console.WriteLine(sl[2]);
+            Console.WriteLine(sl[3]);
+
+            foreach (object key in sl.Keys)
+            {
+                Console.WriteLine($"{key} -- {sl[key]}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
