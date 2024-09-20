ae4b968bc52c31dab8a748e638f9b0eb41f5382f
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 19:11:51 +0530

    palindrome or not.

---
+++ Part-93/Programs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "saasaa";
+            int start = 0;
+            int end = s.Length - 1;
+            while (start <= end)
+            {
+                if (s[start] != s[end])
+                {
+                    Console.WriteLine("String is not palindrome..");
+                    break;
+                   
+                }
+                else
+                {
+                    start++;
+                    end--;
+                }
+            }
+
+            if (start >= end)
+            {
+                Console.WriteLine("palindrome string");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
