cf5a4b7da0af767ae812b89f80b427caee37b28b
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 19:40:46 +0530

    prime no.

---
+++ Part-98/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static bool PrimeOrNot(int num)
+        {
+            if (num == 0 && num == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < num; i++) { 
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number...");
+            int num = int.Parse(Console.ReadLine());
+            if (Program.PrimeOrNot(num))
+            {
+                Console.WriteLine("Prime no..");
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime no..");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
