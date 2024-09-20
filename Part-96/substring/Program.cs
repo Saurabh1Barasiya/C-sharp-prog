d07949247ab8e57ff5fa3d853bc9a1584e0453d6
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 19:16:23 +0530

    find substring.

---
+++ Part-96/substring/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace substring
+{
+    internal class Program
+    {
+        public static string GetSubString()
+        {
+            Console.WriteLine("Enter a string");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter start point");
+            int startIndex = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter start point");
+            int length = int.Parse(Console.ReadLine());
+
+
+            if(startIndex >= 0 && (startIndex + length) <= inputString.Length)
+            {
+                return inputString.Substring(startIndex, length);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string substring = Program.GetSubString();
+            Console.WriteLine(substring);
+            Console.ReadLine();
+        }
+    }
+}
