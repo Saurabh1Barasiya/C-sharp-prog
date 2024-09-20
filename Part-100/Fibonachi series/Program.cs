26ac48353e64dc621fdbab8835bd6fa619f582f9
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 22:24:25 +0530

    fiboncchi series .

---
+++ Part-100/Fibonachi series/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -1; int b = 1;
+
+            int temp;
+
+            int terms = 10;
+
+            for (int i = 0; i < terms; i++)
+            {
+                temp = a + b;
+                Console.Write($" {temp} ");
+                a = b;
+                b = temp;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
