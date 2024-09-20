98076da8c32b6da21dad0fc8c36cede9c9801fdc
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 19:53:45 +0530

    Prime no in a specific range.

---
+++ Part-99/get prime range/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_prime_range
+{
+    internal class Program
+    {
+        public static bool getPrimeRange(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < num; i++) { 
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+         
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++) {
+                if (Program.getPrimeRange(i))
+                {
+                    Console.WriteLine($"Prime number : {i} ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
