a74ff679210121fb227f0b296528c9495c3c2c3f
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-09-20 19:14:15 +0530

    Reverse a string.

---
+++ Part-94/string queston/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_queston
+{
+   
+
+    internal class Program
+    {
+        public static string Reverse(string s1)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int end = s1.Length - 1;
+            while (end >= 0)
+            {
+                stringBuilder.Append(s1[end]);
+                end --;
+            }
+            return stringBuilder.ToString();
+
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "saurabh";
+            string reversestring = Program.Reverse(s);
+            Console.WriteLine(reversestring);
+            Console.ReadLine();
+        }
+    }
+}
